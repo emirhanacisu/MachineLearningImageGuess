0613a16a12da278cd02c42b10a05ffdf344e61da
emirhan acısu <emirhan.acisu@gmail.com>
2019-09-30 22:10:25 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearningImageGuess
+//
+//  Created by Erhan Acisu on 30.09.2019.
+//  Copyright © 2019 Emirhan Acisu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
